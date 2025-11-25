692780a1aa10e7a99cdcf716f4f2a24358692b42
tidalwave <tidalwave560@anglernook.com>
2025-11-24 22:08:16 -0500

    add automatic script

---
+++ install.sh
@@ -0,0 +1,25 @@
+# Instal stow
+sudo apt install stow
+stow .
+
+# Install tmux
+sudo apt install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
+
+# Install nvim
+sudo apt-get install ninja-build gettext cmake curl build-essential git
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd ..
+
+
+# zsh configuration
+sudo apt install zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting/
+chsh -s $(which zsh)
+# restart terminal
